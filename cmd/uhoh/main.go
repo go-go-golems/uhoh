bb3d1466283dff9907cf9d84cf5aded7767636aa
Manuel Odendahl <wesen@ruinwesen.com>
2024-08-04 14:34:20 -0400

    :art: Turn main into a cobra command

--- cmd/uhoh/main.go
+++ cmd/uhoh/main.go
@@ -2,13 +2,21 @@ package main
 
 import (
 	"fmt"
-	"github.com/go-go-golems/uhoh/pkg"
-	"gopkg.in/yaml.v3"
 	"log"
+
+	"github.com/go-go-golems/uhoh/pkg"
+	"github.com/spf13/cobra"
+	"gopkg.in/yaml.v3"
 )
 
-func main() {
-	yamlData := `
+var version = "dev"
+
+var rootCmd = &cobra.Command{
+	Use:     "uhoh",
+	Short:   "uhoh is a tool to help you run forms",
+	Version: version,
+	Run: func(cmd *cobra.Command, args []string) {
+		yamlData := `
 name: Burger Order Form
 theme: Charm
 groups:
@@ -32,22 +40,28 @@ groups:
             error: Sorry, we don't serve customers named Frank.
       - type: confirm
         key: discount
-        title: Would you like 15% off?
+        title: Would you like 15% off?ggj
 `
 
-	var form pkg.Form
-	err := yaml.Unmarshal([]byte(yamlData), &form)
-	if err != nil {
-		log.Fatalf("Error parsing YAML: %v", err)
-	}
+		var form pkg.Form
+		err := yaml.Unmarshal([]byte(yamlData), &form)
+		if err != nil {
+			log.Fatalf("Error parsing YAML: %v", err)
+		}
 
-	values, err := form.Run()
-	if err != nil {
-		log.Fatalf("Error running form: %v", err)
-	}
+		values, err := form.Run()
+		if err != nil {
+			log.Fatalf("Error running form: %v", err)
+		}
 
-	fmt.Println("Form Results:")
-	for key, value := range values {
-		fmt.Printf("%s: %v\n", key, value)
-	}
+		fmt.Println("Form Results:")
+		for key, value := range values {
+			fmt.Printf("%s: %v\n", key, value)
+		}
+	},
+}
+
+func main() {
+	err := rootCmd.Execute()
+	cobra.CheckErr(err)
 }
